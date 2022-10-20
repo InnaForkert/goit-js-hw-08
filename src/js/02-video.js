146e5692f1887c602b91cfdf1206dd1bd581a651
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-21 02:59:18 +0300

    task 2 + import lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,11 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
-player.on("play", function() {
-  console.log("played the video!");
+player.on("timeupdate", function() {
+    player.getCurrentTime().then((seconds) => {
+        localStorage.setItem("videoplayer-current-time", seconds);
+    })
 });
 
-player.getVideoTitle().then(function(title) {
-  console.log("title:", title);
-});
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
