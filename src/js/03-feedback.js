58e5a0ef63415a4fc572fdf0a11146713eb77f9d
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-21 17:50:43 +0300

    all but throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -16,32 +16,10 @@ function saveData(event) {
 form.elements.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
 form.elements.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
 
+form.addEventListener('submit', formSubmit);
 
-// function formSubmit(event) {
-//     event.preventDefault();
-
-//     const {
-//         elements: { email, password }
-//     } = event.currentTarget;
-
-//     if (!email.value || !password.value) {
-//         alert("Усі поля повинні бути заповнені!")
-//     } else {
-//         let formData = {
-//             email: email.value,
-//             password: password.value
-//         }
-//         console.log(formData);
-//         event.currentTarget.reset();
-//     }
-// }
-
-// form.addEventListener('submit', formSubmit)
-
-// const span = document.querySelector("#name-output");
-
-// function changeName() {
-//     span.innerHTML = input.value || "Anonymous";
-// }
-
-// input.addEventListener('input', changeName);
+function formSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log(localStorage.getItem("feedback-form-state"));
+}
