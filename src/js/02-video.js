a25a99afe4a2a60b9195ab5b421c38c5198931ae
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-10-21 17:43:39 +0300

    push while it works

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,4 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
-
 
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
 
+const form = document.querySelector(".feedback-form");
+// const input = document.querySelector("input");
+// const textarea = document.querySelector("textarea");
+
+form.addEventListener('input', saveData);
+
+function saveData(event) {
+    const {
+        elements: { email, message }
+     } = event.currentTarget;
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }));
+    console.log(localStorage.getItem("feedback-form-state"));
+}
+
+
+// function formSubmit(event) {
+//     event.preventDefault();
+
+//     const {
+//         elements: { email, password }
+//     } = event.currentTarget;
+
+//     if (!email.value || !password.value) {
+//         alert("Усі поля повинні бути заповнені!")
+//     } else {
+//         let formData = {
+//             email: email.value,
+//             password: password.value
+//         }
+//         console.log(formData);
+//         event.currentTarget.reset();
+//     }
+// }
+
+// form.addEventListener('submit', formSubmit)
+
+// const span = document.querySelector("#name-output");
+
+// function changeName() {
+//     span.innerHTML = input.value || "Anonymous";
+// }
+
+// input.addEventListener('input', changeName);
