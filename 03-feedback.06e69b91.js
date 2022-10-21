0b35d72c997da579ffbe3f6cd946f0f5f4cc3971
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-21 14:51:17 +0000

    Deploying to gh-pages from @ InnaForkert/goit-js-hw-08@58e5a0ef63415a4fc572fdf0a11146713eb77f9d 🚀

--- 03-feedback.06e69b91.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){const{elements:{email:a,message:t}}=e.currentTarget;localStorage.setItem("feedback-form-state",JSON.stringify({email:a.value,message:t.value}))})),e.elements.email.value=JSON.parse(localStorage.getItem("feedback-form-state")).email,e.elements.message.value=JSON.parse(localStorage.getItem("feedback-form-state")).message;
-//# sourceMappingURL=03-feedback.06e69b91.js.map
