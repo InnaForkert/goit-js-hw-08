b1ef8f5474934a7d2aa50e9d6542f3fea525d204
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-22 12:47:04 +0300

    JSON.parse as a const

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const _ = require("underscore");
+const _ = require('underscore');
 
-player.on("timeupdate", _.throttle(function() {
-    player.getCurrentTime().then((seconds) => {
-        localStorage.setItem("videoplayer-current-time", seconds);
-    })
-}, 1000));
+player.on(
+  'timeupdate',
+  _.throttle(function () {
+    player.getCurrentTime().then(seconds => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000)
+);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
-
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,28 @@
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 
-const _ = require("underscore");
+const _ = require('underscore');
 form.addEventListener('input', _.throttle(saveData, 500));
 
 function saveData(event) {
-    const {
-        elements: { email, message }
-     } = event.currentTarget;
-    
-    localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }));
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: email.value, message: message.value })
+  );
 }
 
-form.elements.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
-form.elements.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
 
 form.addEventListener('submit', formSubmit);
 
 function formSubmit(event) {
-    event.preventDefault();
-    event.currentTarget.reset();
-    console.log(localStorage.getItem("feedback-form-state"));
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
 }
