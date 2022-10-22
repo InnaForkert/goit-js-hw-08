38c8293f77543e29b4b5a6b6b7be8bbe265c3895
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-22 13:12:41 +0300

    moved form elements declaration out of the function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,29 @@
 const form = document.querySelector('.feedback-form');
-
+const {
+  elements: { email, message },
+} = form;
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+  email: '',
+  message: '',
+};
 const _ = require('underscore');
+
 form.addEventListener('input', _.throttle(saveData, 500));
+form.addEventListener('submit', formSubmit);
 
-function saveData(event) {
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+email.value = formData.email;
+message.value = formData.message;
 
+function saveData() {
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify({ email: email.value, message: message.value })
   );
 }
 
-const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-form.elements.email.value = formData.email;
-form.elements.message.value = formData.message;
-
-form.addEventListener('submit', formSubmit);
-
 function formSubmit(event) {
   event.preventDefault();
-  event.currentTarget.reset();
   console.log(localStorage.getItem('feedback-form-state'));
+  event.currentTarget.reset();
+  localStorage.clear();
 }
